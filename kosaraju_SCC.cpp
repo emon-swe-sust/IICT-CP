c9e520602ff391496816742e756ed52c7f1ace96
Mahfuzur Rahman Emon <43314141+emon-swe-sust@users.noreply.github.com>
2024-03-11 00:09:56 +0600

    Use stack in kosaraju

--- kosaraju_SCC.cpp
+++ kosaraju_SCC.cpp
@@ -5,20 +5,19 @@ const int max_node = 1000;
 vector<int> graph[max_node];
 vector<int> transpose_graph[max_node];
 bool vis[max_node];
-
-vector<int> node_order;
+stack<int> st;
 
 void dfs(int n){
     if(vis[n]) return;
 
-    int len= graph[n].size();
-    vis[n]= true;
+    int len=graph[n].size();
+    vis[n]=true;
 
     for(int i= 0; i<len; i++){
         dfs(graph[n][i]);
     }
-
-    node_order.push_back(n);
+    cout << n << " ";
+    st.push(n);
 }
 
 void dfs_print(int n){
@@ -35,27 +34,26 @@ void dfs_print(int n){
 }
 
 int kosarajuSCC(int numberOfNodes){
-    int scc_count= 0;
-    for(int i= 0; i<numberOfNodes; i++){
+    int scc_count=0;
+    for(int i=0; i<numberOfNodes; i++){
         if(vis[i] == false){
             dfs(i);
         }
     }
 
     for(int i=0; i<numberOfNodes; i++){
-        vis[i]= false;
+        vis[i]=false;
     }
 
-    for(int i= node_order.size()-1; i>= 1; i--){
-        if(vis[node_order[i]] == false){
-            dfs_print(node_order[i]);
+    while(!st.empty()){
+        if(vis[st.top()] == false){
+            dfs_print(st.top());
             scc_count++;
             cout<<endl;
         }
-
+        st.pop();
     }
 
-    node_order.clear();
     return scc_count;
 }
 
@@ -70,8 +68,7 @@ int main(){
     }
 
     for(int i= 0; i<numberOfEdges; i++){
-        cin>>u>>v;     //edge u -> v
-
+        cin>>u>>v;
         graph[u].push_back(v);
         transpose_graph[v].push_back(u);
     }
